51f638389015e2d5ee16953912a5ade9b9e78375
Approxamatrix <pokekid919@protonmail.com>
2025-07-09 12:50:41 -0400

    Added unlocks for the blaster and Dash that unlock them for the player when collected.

---
+++ Scripts/Modules/UnlockDash.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	
+	if body.is_in_group("Player"):
+		
+		GameAutoload.HasDashModule = true
+		queue_free()
+		
+		pass
+		
+	
+	pass # Replace with function body.
